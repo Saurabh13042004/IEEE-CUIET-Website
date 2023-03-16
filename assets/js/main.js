48fef162b0fad458ca259970e6aa66b262fb4327
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2023-03-16 11:58:48 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+
 
   /**
    * Sticky Header on Scroll
